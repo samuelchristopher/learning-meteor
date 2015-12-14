ef195ed7c4294b5a153481a7148fa3386a6f5b59
Samuel Christopher <thesamchris@gmail.com>
2015-12-14 08:12:28 +0000

    Created simple todo

--- learning-meteor.js
+++ learning-meteor.js
@@ -1,19 +1,17 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  Template.body.helpers({
+    todos: [
+      {
+        title: 'Todo #1'
+      },
+      {
+        title: 'Todo #2'
+      },
+      {
+        title: 'Todo #3'
+      },
+    ]
+  })
 }
 
 if (Meteor.isServer) {
